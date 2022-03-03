61280b2993d38130b114e26f3c4bcb181dc6d434
drowsell <drowsell@sfu.ca>
2022-03-03 15:49:01 -0800

    Updating documentation for Character.java and Direction.java

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,3 +1,6 @@
+/**
+ * Contains methods that are applicable to all npc and human players.
+ */
 public abstract class Character {
 
     private int health = 0;
@@ -12,6 +15,9 @@ public abstract class Character {
         this.setHealth(health);
     }
 
+    /**
+     * Changes the health of the player.
+     */
     public void setHealth(int health) {
         if (health >= 0 && health <= 100) {
             this.health = health;
@@ -22,30 +28,51 @@ public abstract class Character {
         }
     }
 
+    /**
+     * Returns player's current health.
+     */
     public int getHealth() {
         return health;
     }
 
+    /**
+     * Changes the position of the player.
+     */
     public void setPosition(int[] position) {
         this.position = position;
     }
 
+    /**
+     * Returns player's current position.
+     */
     public int[] getPosition() {
         return this.position;
     }
 
+    /**
+     * Changes the initial position of the player.
+     */
     public void setStartState(int[] startState) {
         this.startState = startState;
     }
 
+    /**
+     * Returns player's original position.
+     */
     public int[] getStartState() {
         return startState;
     }
 
+    /**
+     * Changes the direction the player is facing on the map.
+     */
     public void setDirectionFacing(Direction directionFacing) {
         this.directionFacing = directionFacing;
     }
 
+    /**
+     * Returns player's direction they are facing on the map.
+     */
     public Direction getDirectionFacing() {
         return directionFacing;
     }
--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -1,3 +1,6 @@
+/**
+ * Contains all possible directions a character can face on the map.
+ */
 public enum Direction {
     NORTH, SOUTH, EAST, WEST
 }
