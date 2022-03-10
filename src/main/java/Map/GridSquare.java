7b5702d86923fd1851b11b818da2fc8842783ece
drowsell <drowsell@sfu.ca>
2022-03-09 21:49:05 -0800

    Adding GridSquare.java, GridSquareFactory.java. Adds a basic way to draw the map. Will improve this in later commit.

---
+++ src/main/java/Map/GridSquare.java
@@ -0,0 +1,24 @@
+package Map;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * INFO HERE
+ */
+public class GridSquare {
+    private BufferedImage square;
+
+    /**
+     * INFO HERE
+     */
+    public void setSquare(BufferedImage square) {
+        this.square = square;
+    }
+
+    /**
+     * INFO HERE
+     */
+    public BufferedImage getSquare() {
+        return square;
+    }
+}
