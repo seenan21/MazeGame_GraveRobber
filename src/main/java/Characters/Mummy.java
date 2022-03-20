94c7e92bcc0573964bea5384b1cc381d12434d79
seenan21 <seenan21@gmail.com>
2022-03-19 18:06:56 -0700

    Implemented mummy, please put mummy update and all that in the level and map

--- src/main/java/Characters/Mummy.java
+++ src/main/java/Characters/Mummy.java
@@ -14,6 +14,7 @@ import java.lang.*;
 public class Mummy extends Character {
 
     PlayerActor target;
+    Position position;
 
     public Mummy(Grid grid, Keyboard keyboard, int positionX, int positionY, Level level) {
         super(grid, keyboard, level);
@@ -21,6 +22,7 @@ public class Mummy extends Character {
         this.setStartState(positionX, positionY);
         this.setSpeed(5); //Testing speed
         target = level.getHero();
+        position = new Position(positionX, positionY);
 
     }
 
@@ -29,85 +31,52 @@ public class Mummy extends Character {
         //
     }
 
+    private Position bestMove(Character Hero){
+        int[] t = Hero.getPosition();
+        Position target = new Position(t[0], t[1]);
+        Position p0 = this.position;
+
+
+        Position[] positions = new Position[]{
+                new Position(p0.x + this.getSpeed(), p0.y),
+                new Position(p0.x - this.getSpeed(), p0.y),
+                new Position(p0.x, p0.y + this.getSpeed()),
+                new Position(p0.x, p0.y - this.getSpeed()),
+        };
+
+
+        int min = 9999;
+        Position bestP = positions[0];
+        for(Position p: positions){
+            int num = (int)Math.sqrt((target.y - p.y) * (target.y - p.y) + (target.x - p.x) * (target.x - p.x));
+            if(level.collisionCheck(this, p.x,p.y)){
+                continue;
+            }
+            if (num < min) {
+                min = num;
+                bestP = p;
+            }
+        }
+        //System.out.println(bestP.x);
+        return bestP;
+
+    }
+
+    public void update() {
+
+        Position move = bestMove(level.getHero());
+        this.position = move;
+        this.setPosition(move.x,move.y);
+
+    }
+
     public void render(Graphics g)
     {
         int[] position = this.getPosition();
         g.drawImage(getSprite(Direction.SOUTH), position[0], position[1], null);
     }
 
-    public double euclideanDistance(int ownPosx, int ownPosy, int posX, int posY) {
-       return (Math.sqrt(Math.pow(ownPosx-posX,2) + Math.pow(ownPosy-posY,2)));
 
-    }
-    public double getMinValue(double[] array) {
-        double minValue = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < minValue) {
-                minValue = array[i];
-            }
-        }
-        return minValue;
-    }
-
-    public Position[] getSuccessor(Position p){
-        Position p1 = new Position(p.x + getSpeed(), p.y);
-        Position p2 = new Position(p.x - getSpeed(), p.y);
-        Position p3 = new Position(p.x, p.y + getSpeed());
-        Position p4 = new Position(p.x, p.y - getSpeed());
-        Position[] positions = new Position[4];
-        positions[0] = p1;
-        positions[1] = p2;
-        positions[2] = p3;
-        positions[3] = p4;
-
-        return positions;
-    }
-
-    class PositionList{
-        Position position;
-        ArrayList<Position> posList;
-
-        PositionList(Position pos, ArrayList<Position> list){
-            this.position =pos;
-            this.posList = list;
-        }
-
-    }
-
-    public ArrayList<Position> dfs(Position starting, Position target){
-        Stack<PositionList> fringe = new Stack<>();
-        ArrayList<Position> visited = new ArrayList<Position>();
-        ArrayList<Position> posList = new ArrayList<Position>();
-
-        PositionList root = new PositionList(starting, posList );
-        fringe.push(root);
-        while(!fringe.isEmpty()){
-            PositionList v = fringe.pop();
-
-            if (v.position == target){
-                return v.posList;
-            }
-
-            if (!visited.contains(v.position)){
-                visited.add(v.position);
-                for (Position p: getSuccessor(v.position) ){
-                    PositionList p2 = new PositionList(p, v.posList);
-                    fringe.push(p2);
-                }
-            }
-
-        }
-        return visited;
-    }
-
-    public void update(Character targetCharacter) {
-        Position targetPos = new Position(this.target.getPosition()[0], this.target.getPosition()[1]);
-        Position self = new Position(this.getPosition()[0], this.getPosition()[1]);
-
-        ArrayList<Position> moves = dfs(self, targetPos);
-
-        this.setPosition(moves.get(0).x,moves.get(0).y);
-    }
     public void draw(Graphics2D g2) {
         g2.setColor(Color.WHITE);
         g2.fillRect(this.getPosition()[0],this.getPosition()[1], _grid.getTileSize(), _grid.getTileSize());
