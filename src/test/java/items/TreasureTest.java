697930038265045107b39b683b8c4065fb29382c
Ling-Yu Keng <sky890630@gmail.com>
2022-04-04 12:21:19 -0700

    TreasureTest updated

--- src/test/java/items/TreasureTest.java
+++ src/test/java/items/TreasureTest.java
@@ -1,8 +1,10 @@
 package items;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TreasureTest {
 
@@ -13,12 +15,30 @@ public class TreasureTest {
     public TreasureTest(){treasure = new Treasure(positionX, positionY);
     }
 
-
-
+    /**
+     * Test that the point is 1
+     */
     @Test
-    public void getImageTest() {
-        assertEquals(treasure._image,treasure.getImage());
+    public void testTrapPoint(){
+        assertEquals(1, treasure.getPoints());
     }
 
+    /**
+     * Test that getImage() is working
+     */
+    @Test
+    public void testGetImage() {
+        treasure.getImage();
+    }
+
+    /**
+     * Test that the update() is working
+     */
+    @Test
+    public void testUpdate(){
+        treasure.update();
+        boolean trapAvailable = treasure._available;
+        Assert.assertTrue(trapAvailable);
+    }
 
 }
