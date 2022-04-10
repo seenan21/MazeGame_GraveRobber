4fb2116d9cb9f5a80bb3a413174df17f8e583806
graykeng <graykeng7@gmail.com>
2022-04-09 19:56:47 -0700

    CODE DUPLICATION: Making String soundPath[] to store the paths of the sound effect and the BGM instead of directly store to URL  soundURL[].

--- src/main/java/Map/Sound.java
+++ src/main/java/Map/Sound.java
@@ -12,20 +12,25 @@ public class Sound {
     URL soundURL[] = new URL[20];
 
     public Sound() {
-        soundURL[0] = getClass().getResource("/sound/music.wav");
-        soundURL[1] = getClass().getResource("/sound/regHeart.wav");
-        soundURL[2] = getClass().getResource("/sound/bigHeart.wav");
-        soundURL[3] = getClass().getResource("/sound/enter.wav");
-        soundURL[4] = getClass().getResource("/sound/start.wav");
-        soundURL[5] = getClass().getResource("/sound/lost.wav");
-        soundURL[6] = getClass().getResource("/sound/zombieAttack.wav");
-        soundURL[7] = getClass().getResource("/sound/random_1.wav");
-        soundURL[8] = getClass().getResource("/sound/random_2.wav");
-        soundURL[9] = getClass().getResource("/sound/random_3.wav");
-        soundURL[10] = getClass().getResource("/sound/random_4.wav");
-        soundURL[11] = getClass().getResource("/sound/random_5.wav");
-        soundURL[12] = getClass().getResource("/sound/random_6.wav");
-        soundURL[13] = getClass().getResource("/sound/hurt.wav");
+        String soundPath[] = {
+            "/sound/music.wav",
+            "/sound/regHeart.wav",
+            "/sound/bigHeart.wav",
+            "/sound/enter.wav",
+            "/sound/start.wav",
+            "/sound/lost.wav",
+            "/sound/zombieAttack.wav",
+            "/sound/random_1.wav",
+            "/sound/random_2.wav",
+            "/sound/random_3.wav",
+            "/sound/random_4.wav",
+            "/sound/random_5.wav",
+            "/sound/random_6.wav",
+            "/sound/hurt.wav"
+        };
+        for (int i = 0; i < soundPath.length; i++){
+            soundURL[i] = getClass().getResource(soundPath[i]);
+        }
     }
 
     /**
